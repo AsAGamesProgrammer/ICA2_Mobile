8e309983b57aa31a759366ffe1a352727dfaa2a2
Unknown <misskristinablinova@gmail.com>
2017-11-20 14:18:46 +0000

    Fighting view set up
    - Sprites rearanged
    - View controllers added

---
+++ ICA2KristinaBlinova/ICA2KristinaBlinova/VCFight.h
@@ -0,0 +1,13 @@
+//
+//  VCFight.h
+//  ICA2KristinaBlinova
+//
+//  Created by BLINOVA, KRISTINA on 20/11/2017.
+//  Copyright © 2017 BLINOVA, KRISTINA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCFight : UIViewController
+
+@end
---
+++ ICA2KristinaBlinova/ICA2KristinaBlinova/VCMainMenu.h
@@ -0,0 +1,23 @@
+//
+//  VCMainMenu.h
+//  ICA2KristinaBlinova
+//
+//  Created by BLINOVA, KRISTINA on 20/11/2017.
+//  Copyright © 2017 BLINOVA, KRISTINA. All rights reserved.
+//
+
+
+/*
+ 
+ REFERENCES
+ 
+ Orc sprites: https://free-game-assets.itch.io/free-2d-orcs-sprite-sheets
+ 
+ 
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface VCMainMenu : UIViewController
+
+@end
